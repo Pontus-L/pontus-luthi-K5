dc758d099fd63bf99922ff7b01b91fbe376e4604
Brontus <35817642+Brontus@users.noreply.github.com>
2020-03-23 09:41:12 +0100

    Added products page including start of js (not finished), minor fixes

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,19 @@
 let cart = [];
+let products = document.querySelectorAll(".products > div > h1");
+let htmlCart = document.querySelector(".cart");
+
+for (let i = 0; i < products.length; i++) {
+  products[i].parentElement.addEventListener("click", () => {
+    addToCart(products[i]);
+  });
+}
+
+function addToCart(element) {
+  cart.push(element.innerHTML);
+  let content = "";
+  for (let i = 0; i < cart.length; i++) {
+    content += cart[i];
+  }
+
+  htmlCart.innerHTML = content;
+}
