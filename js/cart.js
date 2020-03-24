65f749ddbd71988fba0277a230a406fed5671966
unknown <pontus.luthi@gmail.com>
2020-03-24 13:25:04 +0100

    Added cart functionality in cart.js" art.js

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,8 @@
 let cart = [];
+
 let products = document.querySelectorAll(".products > div > h1");
-let htmlCart = document.querySelector(".cart");
+let htmlCart = document.querySelector(".cart > ul");
+sessionStorage.setItem("totalCost", 0);
 
 for (let i = 0; i < products.length; i++) {
   products[i].parentElement.addEventListener("click", () => {
@@ -9,11 +11,46 @@ for (let i = 0; i < products.length; i++) {
 }
 
 function addToCart(element) {
-  cart.push(element.innerHTML);
-  let content = "";
+  if (checkForItem(element)) {
+    editItem(element);
+  } else {
+    cart.push({item: element.innerHTML, price: 10, quantity: 1});
+    renderAllItems();
+  }
+}
+
+function checkForItem(element) {
+  console.log(cart.length)
+  if (cart.length > 0) {
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i].item == element.innerHTML) 
+      {
+        return true;
+      } 
+    }
+  } else {
+    return false;
+  }
+}
+
+function renderAllItems() {
+  htmlCart.innerHTML = "";
   for (let i = 0; i < cart.length; i++) {
-    content += cart[i];
+    let cartItem = document.createElement("li");
+    cartItem.innerHTML = cart[i].item + "  price: " + (cart[i].price * cart[i].quantity) + "  quantity: " + cart[i].quantity;
+    htmlCart.appendChild(cartItem);
+  }
+}
+
+function editItem(element) {
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].item == element.innerHTML) 
+    {
+      cart[i].quantity += 1;
+      renderAllItems();
+    } 
   }
 
-  htmlCart.innerHTML = content;
+  
 }
